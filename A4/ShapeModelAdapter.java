90865be053c139a590d1266ddb0511c0de0acda5
ccor065 <ccor065@aucklanduni.ac.nz>
2021-10-24 15:41:46 +1300

    Extended implemenation for ShapeModelAdapter.java, code runner q3

--- A4/ShapeModelAdapter.java
+++ A4/ShapeModelAdapter.java
@@ -1,13 +1,58 @@
 /*
  *	===============================================================================
- *	ShapeModelAdapter.java : Generates a table reporesenting the innerShapes
+ *	ShapeModelAdapter.java : Generates a table representing the innerShapes
  *  YOUR UPI: CCOR065, all Implementation
  *	=============================================================================== */
 
 // import java.util.*;
-class ShapeModelAdapter extends AbstractTableModel {
+class ShapeModelAdapter extends AbstractTableModel implements TreeModel  {
   private String[] columnNames = new String[] {"Type", "Width", "Height"};
+  private ArrayList<TreeModelListener> treeModelListeners  = new ArrayList<TreeModelListener>();
+
   public ShapeModelAdapter(){}
+
+
+  public Shape getRoot(){return root;}
+  public boolean isLeaf(Object node){
+      if (node instanceof NestedShape){
+          return false;
+      }
+      ArrayList<Shape> innerShapes = selectedNestedShape.getAllInnerShapes();
+      for (int i = 0; i < innerShapes.size(); i++){
+          if(innerShapes.get(i) == node){
+              return true;
+          }
+      }
+    return false;
+
+  }
+  public Object getChild(Object parent, int index){
+      if(parent instanceof NestedShape){
+          ArrayList<Shape> innerShapes = ((NestedShape)parent).getAllInnerShapes();
+          if (index < ((NestedShape)parent).getSize() && index>= 0){
+              return innerShapes.get(index);
+          }
+      }
+    return null;
+
+  }
+
+  public int getChildCount(Object parent){
+      if(parent instanceof NestedShape){
+         return ((NestedShape)parent).getSize();
+      }
+      return 0;
+  }
+  public int getIndexOfChild(Object parent, Object child) {
+      if(parent instanceof NestedShape && child instanceof Shape){
+          return ((NestedShape)parent).indexOf((Shape)child);
+      }
+      return -1;
+
+  }
+  public void valueForPathChanged(TreePath path, Object newValue){}
+  public void  addTreeModelListener(final TreeModelListener tml) {treeModelListeners.add(tml);}
+  public void  removeTreeModelListener(final TreeModelListener tml) {treeModelListeners.remove(tml);}
   public int getColumnCount(){return this.columnNames.length;}
   public int getRowCount(){return selectedNestedShape.getSize();}
   public String getColumnName(int column){return this.columnNames[column];}
